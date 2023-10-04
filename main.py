af22428243308e2864b65d6b2512c733906a82de
Артём Тюрин <zientenin@mail.ru>
2023-10-04 13:50:10 +0300

    finish degree function logic

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # review for:
 # Kolkareva
 # Kuznetsova
+PI = 3.14
+
+
 def degree(radius: float,
            time: float,
            acceleration: float,
            velocity: float = 0) -> float:
-    spatium = velocity*time+((acceleration*time**2)/2)
+    spatium = velocity * time + ((acceleration * time ** 2) / 2)
+    length = 2 * PI * radius
+    return (spatium % length) / length * 360
