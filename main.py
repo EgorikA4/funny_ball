aa63a796dbe1a43ed1e50c175389308aacca956b
Артём Тюрин <zientenin@mail.ru>
2023-10-04 14:07:35 +0300

    Added docstring for a function

--- main.py
+++ main.py
@@ -4,10 +4,18 @@
 # Kuznetsova
 
 
-def degree(radius: float,
-           time: float,
-           acceleration: float,
-           velocity: float = 0) -> float:
+def degree(radius: float, time: float, acceleration: float, velocity: float = 0) -> float:
+    """Calculate the angle of rotation of the ball.
+
+    Args:
+        radius: float - ball radius.
+        time: float - moment in time.
+        acceleration: float - ball acceleration.
+        velocity: float - start ball speed.
+
+    Returns:
+        float - Rotation angle of the starting point.
+    """
     spatium = velocity * time + ((acceleration * time ** 2) / 2)
     length = 2 * 3.14 * radius
     return (spatium % length) / length * 360
