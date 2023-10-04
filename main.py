1cc665e37d8ea60438cc7734e671529a8c815e3d
EgorikA4 <litvinov19801985@mail.ru>
2023-10-04 14:34:11 +0300

    remove main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# review for:
-# Kolkareva
-# Kuznetsova
-def degree(radius: float,
-           time: float,
-           acceleration: float,
-           velocity: float = 0) -> float:
-    spatium = velocity*time+((acceleration*time**2)/2)
